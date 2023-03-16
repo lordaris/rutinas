4a19b79a912900ce14a56bc574854ca9ffdccbb2
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2023-03-16 12:16:35 -0600

    Second

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,17 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
-
+import pochitawaving from "../../public/pochita.jpg";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+
+        <Image alt={"pochita"} src={pochitawaving} width={200} height={200} />
       <h1 className="text-3xl font-bold mb-4">
         Bienvenido a mi app de rutinas.
       </h1>
-      <p className="text-lg text-center mb-8">
+
+        <p className="text-lg text-center mb-8">
        Hice esta aplicación para poder enviarte rutinas y que puedas verlas en tu celular.
        Aún es una versión beta, pero espero que te guste.
 
