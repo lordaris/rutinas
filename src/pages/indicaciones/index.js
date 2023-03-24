fb86c6e63ccc6818dfd22ea96d76a9eb476be90c
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2023-03-24 13:44:07 -0600

    Añadí una lista de indicaciones generales

--- src/pages/indicaciones/index.js
+++ src/pages/indicaciones/index.js
@@ -5,14 +5,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Head>
-        <title>Indicaciones para hacer ejercicios</title>
+        <title>Indicaciones generales</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-4xl font-bold">
-          Indicaciones para hacer ejercicios
-        </h1>
+        <h1 className="text-4xl font-bold">Indicaciones generales</h1>
         <p className="mt-8 text-xl">
           <span className={"font-bold"}>Calentar</span> por lo menos 5 minutos
           antes de tu rutina.
@@ -27,6 +25,37 @@ export default function Home() {
           únicamente las repeticiones indicadas. Si se pueden hacer mas
           repeticiones, aumentar el peso, si no se puede un peso mas alto,
           aumentar las repeticiones.
+        </p>
+        <p className={"mt-4 text-xl"}>
+          Hacer{" "}
+          <Link
+            href={
+              "https://inuba.com/blog/rutina-calentamiento-ejercicios-fuerza/"
+            }
+          >
+            <span className={"font-bold text-cyan-800"}>
+              series de aproximación
+            </span>
+          </Link>
+          siempre que sea posible
+        </p>
+        <p className="mt-4 text-xl">
+          {" "}
+          Entender la{" "}
+          <Link
+            href={
+              "https://www.hsph.harvard.edu/nutritionsource/high-intensity-interval-training/"
+            }
+          >
+            <span className={"font-bold text-cyan-800"}>
+              conexión mente-músculo
+            </span>{" "}
+          </Link>{" "}
+          puede ayudarte a mejorar los resultados.
+        </p>
+        <p className={"mt-4 text-xl"}>
+          Hacer <span className={"font-bold"}>descansos cortos </span> siempre
+          que sea posible (especialmente cuando realmente no hay cansancio).
         </p>
         <p className="mt-4 text-xl">
           Seguir con la cadencia para tener mejores resultados.
